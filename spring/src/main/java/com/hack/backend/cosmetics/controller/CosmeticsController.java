8ebb28b3c60ee2281f9efe45b78fe66f11af901c
1224kang <cherry1224@sookmyung.ac.kr>
2024-11-08 18:40:55 +0900

    feat:gradle 버전 수정

--- spring/src/main/java/com/hack/backend/cosmetics/controller/CosmeticsController.java
+++ spring/src/main/java/com/hack/backend/cosmetics/controller/CosmeticsController.java
@@ -1,6 +1,7 @@
 package com.hack.backend.cosmetics.controller;
 
 import com.hack.backend.cosmetics.dto.RoutineDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,14 +13,14 @@ public class CosmeticsController {
 
 //    @Autowired
 //    private final UserRepository userRepository;
-
+//
 //    @GetMapping("/recommend/{user_id}")
 //    public List<RoutineDto> getRoutineRecommend (@PathVariable Long user_id){
 //        User user=UserRepository.findById(user_id);
 //        int usertype=user.getType();
-//
-//
-//    }
+
+
+    }
 
 
 }
