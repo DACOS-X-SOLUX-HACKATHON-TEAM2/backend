00f81678de72f308c0083fc0dda325f5cb16ed80
yu-k-m <karen0806@naver.com>
2024-11-08 18:33:23 +0900

    feat: User Survey 서비스 기능 적용 전

---
+++ spring/src/main/java/com/hack/backend/survey/SurveyService.java
@@ -0,0 +1,7 @@
+package com.hack.backend.survey;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SurveyService {
+}
---
+++ spring/src/main/java/com/hack/backend/user/UserService.java
@@ -0,0 +1,10 @@
+package com.hack.backend.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public User join(User user) {
+        return user;
+    }
+}
